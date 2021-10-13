f91b39f723077e02644db6534cdd99c0e052e86c
adriElias <34091624+adriElias@users.noreply.github.com>
2021-10-13 12:32:40 +0200

    organizar las carpetas

---
+++ Portfolio/cv.php
@@ -0,0 +1,4 @@
+
+ <div>
+    <embed src="Portfolio/media/cv-AdrianaElias.pdf" type="application/pdf" width="100%" height="600px" />
+</div>
--- cv.php
+++
@@ -1,4 +0,0 @@
-
- <div>
-    <embed src="cv-AdrianaElias.pdf" type="application/pdf" width="100%" height="600px" />
-</div>
